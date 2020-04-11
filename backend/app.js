ba001670f7cc7c69fdc468258ceed40f71bdbbca
Alice Post <alicep2322@gmail.com>
2020-04-10 23:26:36 -0600

    Update headers to address CORS errors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  )
+  next()
+});
 
 app.get('/api/posts', (req, res, next) => {
   const posts = [
