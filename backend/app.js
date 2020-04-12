7c7f2c7c896d5ab3280dfb9b32db6214edab3aa8
Alice Post <alicep2322@gmail.com>
2020-04-12 00:46:42 -0600

    Querying db and transforming data response with map

--- backend/app.js
+++ backend/app.js
@@ -44,14 +44,14 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {id: '1', title: 'first server-side post', content: 'this is coming from the server'},
-    {id: '2', title: 'second server-side post', content: 'this is coming from the server!'}
-  ]
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-  })
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: documents
+      })
+    })
+    .catch((error) => console.log(error))
 });
 
 module.exports = app;
