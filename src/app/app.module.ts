0c3906c7599d8d322d9a3c42e3363f9cfa09f905
Alice Post <alicep2322@gmail.com>
2020-04-12 14:57:28 -0600

    Add angular routing that connect a url to the component to load and update in header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,9 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { HeaderComponent } from './header/header.component'
-import { PostListComponent } from './posts/post-list/post-list.component'
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { PostListComponent } from './posts/post-list/post-list.component'
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     NoopAnimationsModule,
     MatInputModule,
